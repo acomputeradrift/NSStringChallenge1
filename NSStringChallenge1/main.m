64844077b802272530093eb17242f0381f5c37cc
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-06 13:06:00 -0700

    Cleaned up the code and commented

--- NSStringChallenge1/main.m
+++ NSStringChallenge1/main.m
@@ -11,13 +11,11 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        char string1[20];
+        char string1[20]; //create the char array for the input from scanf
         NSLog(@"Please enter the number to countdown from:" );
-        scanf("%s", string1);
-        printf("The number is %s\n", string1);
-        NSString *convertedString = [NSString stringWithUTF8String:string1];
-        int convertedInt = [convertedString intValue];
-        NSLog(@"The number is : %@", convertedString );
+        scanf("%s", string1);//fill the variable with the char entered
+        NSString *convertedString = [NSString stringWithUTF8String:string1];//converts the string with the method from a char array to a NSString
+        int convertedInt = [convertedString intValue];//Converts the NSString to an int for use in the for loop
         for (int i = convertedInt; i > 0; i=i-3){
             if (i % 5 == 0) {
                 printf("Found one\n");
