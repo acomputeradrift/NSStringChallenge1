e65a80ce5a6d5a30643a391c3355a9df45bdf4da
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-06 12:59:41 -0700

    Finally works, what a pain in the butt

--- NSStringChallenge1/main.m
+++ NSStringChallenge1/main.m
@@ -8,14 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        for (int i = 100; i > 0; i=i-3){
+        char string1[20];
+        NSLog(@"Please enter the number to countdown from:" );
+        scanf("%s", string1);
+        printf("The number is %s\n", string1);
+        NSString *convertedString = [NSString stringWithUTF8String:string1];
+        int convertedInt = [convertedString intValue];
+        NSLog(@"The number is : %@", convertedString );
+        for (int i = convertedInt; i > 0; i=i-3){
             if (i % 5 == 0) {
                 printf("Found one\n");
             }
             else
-            printf("%d. Jamie is Cool\n", i);
+                printf("%d. Jamie is Cool\n", i);
         }
     }
     return 0;
